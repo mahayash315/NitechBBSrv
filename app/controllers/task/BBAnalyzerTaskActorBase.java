d46ba71c2c852cff5a803a6100254f22e9b96fc7
TopGear <TopGear@mycompany.com>
2014-05-24 18:16:21 +0000

    git-svn-id: https://mattari.mydns.jp/repos/android_apps/trunk/NitechBBSrv@534 ebda3f91-3b32-42d3-bb0a-b50f2d3b12aa

--- app/controllers/task/BBAnalyzerTaskActorBase.java
+++ app/controllers/task/BBAnalyzerTaskActorBase.java
@@ -29,7 +29,18 @@ public class BBAnalyzerTaskActorBase {
 	}
 	
 	public void start() throws ParseException {
-		Time.CronExpression cron = new Time.CronExpression("0 4,11,18 * * * ?");
+		// 定期実行の日付・時刻を設定する
+		Time.CronExpression cron = new Time.CronExpression("0 0 4,11,18 * * * *");
+//		[ Cron Expression ]
+//		Field Name	 	Allowed Values	 	Allowed Special Characters
+//		Seconds	 	0-59	 	, - * /
+//		Minutes	 	0-59	 	, - * /
+//		Hours	 	0-23	 	, - * /
+//		Day-of-month	 	1-31	 	, - * ? / L W
+//		Month	 	1-12 or JAN-DEC	 	, - * /
+//		Day-of-Week	 	1-7 or SUN-SAT	 	, - * ? / L #
+//		Year (Optional)	 	empty, 1970-2099	 	, - * /
+		
 		Date nextValidTimeAfter = cron.getNextValidTimeAfter(new Date());
 		FiniteDuration finiteDuraion = Duration.create(
 				nextValidTimeAfter.getTime() - System.currentTimeMillis(),
