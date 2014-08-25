efa3c60cfc472e6a6025a7bbdd2e6c819ae822f1
TopGear <TopGear@mycompany.com>
2014-08-25 23:31:56 +0000

    git-svn-id: https://mattari.mydns.jp/repos/android_apps/trunk/NitechBBSrv@658 ebda3f91-3b32-42d3-bb0a-b50f2d3b12aa

--- app/models/service/bb/UserClassifier.java
+++ app/models/service/bb/UserClassifier.java
@@ -15,21 +15,20 @@ public class UserClassifier {
 	
 
 	/**
-	 * 第 1 層以上のクラスタを初期化する（作りなおす）
+	 * 第 1 層以上のクラスタを初期化する（クラスタを作りなおす）
 	 */
 	public void initClusters() {
-
 		Ebean.beginTransaction();
 		try {
 			for (int i = 0; i < BBSetting.CLUSTER_SIZES.length; ++i) {
 				CallableSql cSql1 = Ebean.createCallableSql("{call InitKMeansFor(?,?)}");
 				cSql1.setParameter(1, i+1);
 				cSql1.setParameter(2, BBSetting.CLUSTER_SIZES[i]);
-				int r1 = Ebean.execute(cSql1);
+				Ebean.execute(cSql1);
 				
 				CallableSql cSql2 = Ebean.createCallableSql("{call ClassifyClustersFor(?)}");
 				cSql2.setParameter(1, i+1);
-				int r2 = Ebean.execute(cSql2);
+				Ebean.execute(cSql2);
 			}
             Ebean.execute(Ebean.createCallableSql("commit;"));
 			Ebean.commitTransaction();
@@ -40,6 +39,25 @@ public class UserClassifier {
 		} finally {
 			Ebean.endTransaction();
 		}
-		
+	}
+	
+	
+	/**
+	 * 第 0 層から最上位層までクラスタ分析する
+	 */
+	public void classify() {
+		Ebean.beginTransaction();
+		try {
+			CallableSql cSql = Ebean.createCallableSql("{call ClassifyClusters()}");
+			Ebean.execute(cSql);
+            Ebean.execute(Ebean.createCallableSql("commit;"));
+			Ebean.commitTransaction();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Logger.error(UserClassifier.class.getName()+"#initClusters()", e);
+			Ebean.rollbackTransaction();
+		} finally {
+			Ebean.endTransaction();
+		}
 	}
 }
