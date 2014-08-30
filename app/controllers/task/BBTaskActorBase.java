9d41c89e75e457e1280e4161131d8a0ff4075328
TopGear <TopGear@mycompany.com>
2014-08-30 16:31:23 +0000

    git-svn-id: https://mattari.mydns.jp/repos/android_apps/trunk/NitechBBSrv@672 ebda3f91-3b32-42d3-bb0a-b50f2d3b12aa

--- app/controllers/task/BBTaskActorBase.java
+++ app/controllers/task/BBTaskActorBase.java
@@ -31,15 +31,15 @@ public class BBTaskActorBase {
 		// 定期実行の日付・時刻を設定する
 		Time.CronExpression cron = new Time.CronExpression("0 0 4,11,18 * * ? *");
 ////		[ Cron Expression ]
-////			found at : http://www.playframework.com/documentation/2.2.x/api/java/play/libs/Time.CronExpression.html
+////			see http://www.playframework.com/documentation/2.2.x/api/java/play/libs/Time.CronExpression.html
 ////		Field Name	 	Allowed Values	 	Allowed Special Characters
-////		Seconds	 	0-59	 	, - * /
-////		Minutes	 	0-59	 	, - * /
-////		Hours	 	0-23	 	, - * /
-////		Day-of-month	 	1-31	 	, - * ? / L W
-////		Month	 	1-12 or JAN-DEC	 	, - * /
+////		Seconds	 		0-59	 			, - * /
+////		Minutes	 		0-59	 			, - * /
+////		Hours	 		0-23	 			, - * /
+////		Day-of-month	1-31	 			, - * ? / L W
+////		Month	 		1-12 or JAN-DEC	 	, - * /
 ////		Day-of-Week	 	1-7 or SUN-SAT	 	, - * ? / L #
-////		Year (Optional)	 	empty, 1970-2099	 	, - * /
+////		Year (Optional)	empty, 1970-2099	, - * /
 //		
 		Date nextValidTimeAfter = cron.getNextValidTimeAfter(new Date());
 		FiniteDuration finiteDuraion = Duration.create(
