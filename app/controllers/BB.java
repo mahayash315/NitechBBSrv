ca89dcd7966bdb583f95cbb56f531a39c89b976a
TopGear <TopGear@mycompany.com>
2014-09-24 15:26:54 +0000

    BB HTMLページを追加
    git-svn-id: https://mattari.mydns.jp/repos/android_apps/trunk/NitechBBSrv@795 ebda3f91-3b32-42d3-bb0a-b50f2d3b12aa

--- app/controllers/BB.java
+++ app/controllers/BB.java
@@ -1,7 +1,97 @@
 package controllers;
 
+import java.util.List;
+
+import models.entity.Configuration;
+import models.entity.NitechUser;
+import models.entity.bb.Post;
+import models.service.bb.PostClassifier;
+import models.service.bb.UserClassifier;
+import models.setting.BBSetting;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class BB extends Controller {
+	
+	public static Result redirectToIndex() {
+		return redirect(controllers.routes.BB.index());
+	}
 
+	public static Result index() {
+		return ok(views.html.bb.index.render());
+	}
+	
+	public static Result init() {
+    	UserClassifier userClassifier = new UserClassifier();
+    	
+    	// K-Means クラスタを初期化する
+		userClassifier.initClusters();
+		Configuration.putBoolean(BBSetting.CONFIGURATION_KEY_REQUIRE_USER_CLUSTER_INITIALIZATION, false);
+		
+		return ok("OK");
+	}
+	
+	public static Result classify() {
+    	UserClassifier userClassifier = new UserClassifier();
+    	
+    	// ユーザを分類する
+    	userClassifier.classify();
+    	
+    	return ok("OK");
+	}
+	
+	public static Result extract() {
+    	PostClassifier postClassifier = new PostClassifier();
+    	
+		// 単語リストの最終更新日時
+		long lastModified = Configuration.getLong(BBSetting.CONFIGURATION_KEY_WORD_LIST_LAST_MODIFIED, 0L);
+		
+		// 各掲示の特徴量算出結果が古ければ更新
+		List<Post> posts = new Post().findList();
+		for (Post post : posts) {
+			long lastUpdated = post.getLastModified().getTime();
+			if (lastUpdated < lastModified) {
+				postClassifier.calcPostFeature(post);
+			}
+		}
+    	
+    	// 掲示距離を計算する
+    	postClassifier.calcPostDistances();
+    	
+    	return ok("OK");
+	}
+	
+	public static Result train() {
+    	PostClassifier postClassifier = new PostClassifier();
+    	
+    	// パラメータを学習する
+    	postClassifier.train();
+    	
+		return ok("OK");
+	}
+	
+	public static Result estimate() {
+    	PostClassifier postClassifier = new PostClassifier();
+
+    	// 掲示を分類する
+    	List<NitechUser> users = new NitechUser().findList();
+    	for (NitechUser user : users) {
+    		List<Post> posts = user.findPossessingPosts();
+    		for (Post post : posts) {
+    			postClassifier.estimate(user, post);
+    		}
+    	}
+    	
+    	return ok("OK");
+	}
+	
+	public static Result process() {
+		init();
+		classify();
+		extract();
+		train();
+		estimate();
+		
+		return ok("OK");
+	}
 }
